08e7022ad9c0a7e7b7bc7b0691ac87333cf9042d
lucas <lucas@snpsgroup.com>
2019-04-29 15:47:08 -0300

    Projeto padrão para React

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+
+// eslint-disable-next-line no-unused-vars
+class App extends Component {
+  render () {
+    return <h1>Hello World</h1>
+  }
+}
+
+render(<App />, document.getElementById('app'))
